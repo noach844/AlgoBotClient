cc4854d6f097799d2d16fe7955dcce5e11a03b33
noach844 <68166931+noach844@users.noreply.github.com>
2022-04-24 19:10:27 +0300

    done designing

---
+++ AlgoClient/MainForm.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace AlgoClient
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.MenuPanel = new System.Windows.Forms.Panel();
+            this.LogoPanel = new System.Windows.Forms.Panel();
+            this.TitlePanel = new System.Windows.Forms.Panel();
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.WorkspacePanel = new System.Windows.Forms.Panel();
+            this.MaxButton = new System.Windows.Forms.Button();
+            this.MinButton = new System.Windows.Forms.Button();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.AddBotButton = new System.Windows.Forms.Button();
+            this.MenuPanel.SuspendLayout();
+            this.TitlePanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MenuPanel
+            // 
+            this.MenuPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.MenuPanel.Controls.Add(this.button1);
+            this.MenuPanel.Controls.Add(this.AddBotButton);
+            this.MenuPanel.Controls.Add(this.LogoPanel);
+            this.MenuPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.MenuPanel.Location = new System.Drawing.Point(0, 0);
+            this.MenuPanel.Name = "MenuPanel";
+            this.MenuPanel.Size = new System.Drawing.Size(220, 557);
+            this.MenuPanel.TabIndex = 0;
+            // 
+            // LogoPanel
+            // 
+            this.LogoPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(58)))));
+            this.LogoPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.LogoPanel.Location = new System.Drawing.Point(0, 0);
+            this.LogoPanel.Name = "LogoPanel";
+            this.LogoPanel.Size = new System.Drawing.Size(220, 80);
+            this.LogoPanel.TabIndex = 0;
+            // 
+            // TitlePanel
+            // 
+            this.TitlePanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(150)))), ((int)(((byte)(136)))));
+            this.TitlePanel.Controls.Add(this.MaxButton);
+            this.TitlePanel.Controls.Add(this.MinButton);
+            this.TitlePanel.Controls.Add(this.CloseButton);
+            this.TitlePanel.Controls.Add(this.TitleLabel);
+            this.TitlePanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TitlePanel.Location = new System.Drawing.Point(220, 0);
+            this.TitlePanel.Name = "TitlePanel";
+            this.TitlePanel.Size = new System.Drawing.Size(750, 80);
+            this.TitlePanel.TabIndex = 1;
+            this.TitlePanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TitlePanel_MouseDown);
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TitleLabel.ForeColor = System.Drawing.Color.White;
+            this.TitleLabel.Location = new System.Drawing.Point(349, 21);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(83, 32);
+            this.TitleLabel.TabIndex = 0;
+            this.TitleLabel.Text = "Home";
+            // 
+            // WorkspacePanel
+            // 
+            this.WorkspacePanel.Location = new System.Drawing.Point(220, 80);
+            this.WorkspacePanel.Name = "WorkspacePanel";
+            this.WorkspacePanel.Size = new System.Drawing.Size(750, 477);
+            this.WorkspacePanel.TabIndex = 2;
+            // 
+            // MaxButton
+            // 
+            this.MaxButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.MaxButton.FlatAppearance.BorderSize = 0;
+            this.MaxButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.MaxButton.Font = new System.Drawing.Font("Segoe UI", 15F);
+            this.MaxButton.Image = ((System.Drawing.Image)(resources.GetObject("MaxButton.Image")));
+            this.MaxButton.Location = new System.Drawing.Point(681, 3);
+            this.MaxButton.Name = "MaxButton";
+            this.MaxButton.Size = new System.Drawing.Size(30, 30);
+            this.MaxButton.TabIndex = 3;
+            this.MaxButton.UseVisualStyleBackColor = true;
+            this.MaxButton.Click += new System.EventHandler(this.MaxButton_Click);
+            // 
+            // MinButton
+            // 
+            this.MinButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.MinButton.FlatAppearance.BorderSize = 0;
+            this.MinButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.MinButton.Font = new System.Drawing.Font("Segoe UI", 15F);
+            this.MinButton.Image = global::AlgoClient.Properties.Resources.minmize;
+            this.MinButton.Location = new System.Drawing.Point(645, 3);
+            this.MinButton.Name = "MinButton";
+            this.MinButton.Size = new System.Drawing.Size(30, 30);
+            this.MinButton.TabIndex = 2;
+            this.MinButton.UseVisualStyleBackColor = true;
+            this.MinButton.Click += new System.EventHandler(this.MinButton_Click);
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.CloseButton.FlatAppearance.BorderSize = 0;
+            this.CloseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CloseButton.Font = new System.Drawing.Font("Segoe UI", 15F);
+            this.CloseButton.Image = global::AlgoClient.Properties.Resources.close;
+            this.CloseButton.Location = new System.Drawing.Point(717, 3);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(30, 30);
+            this.CloseButton.TabIndex = 1;
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Gainsboro;
+            this.button1.Image = global::AlgoClient.Properties.Resources.AddIcon3;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(0, 140);
+            this.button1.Name = "button1";
+            this.button1.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.button1.Size = new System.Drawing.Size(220, 60);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Add Bot";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // AddBotButton
+            // 
+            this.AddBotButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.AddBotButton.FlatAppearance.BorderSize = 0;
+            this.AddBotButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AddBotButton.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddBotButton.ForeColor = System.Drawing.Color.Gainsboro;
+            this.AddBotButton.Image = global::AlgoClient.Properties.Resources.AddIcon3;
+            this.AddBotButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.AddBotButton.Location = new System.Drawing.Point(0, 80);
+            this.AddBotButton.Name = "AddBotButton";
+            this.AddBotButton.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.AddBotButton.Size = new System.Drawing.Size(220, 60);
+            this.AddBotButton.TabIndex = 1;
+            this.AddBotButton.Text = "Add Bot";
+            this.AddBotButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.AddBotButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.AddBotButton.UseVisualStyleBackColor = true;
+            this.AddBotButton.Click += new System.EventHandler(this.AddBotButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(970, 557);
+            this.Controls.Add(this.WorkspacePanel);
+            this.Controls.Add(this.TitlePanel);
+            this.Controls.Add(this.MenuPanel);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.MenuPanel.ResumeLayout(false);
+            this.TitlePanel.ResumeLayout(false);
+            this.TitlePanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel MenuPanel;
+        private System.Windows.Forms.Panel LogoPanel;
+        private System.Windows.Forms.Button AddBotButton;
+        private System.Windows.Forms.Panel TitlePanel;
+        private System.Windows.Forms.Label TitleLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel WorkspacePanel;
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.Button MaxButton;
+        private System.Windows.Forms.Button MinButton;
+    }
+}
+
---
+++ AlgoClient/MainForm.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlgoClient
+{
+    public partial class MainForm : Form
+    {
+        //Fields
+        private Button currentButton;
+        private Random random;
+        private int tempIndex;
+        private Form activeForm;
+
+        //Constructor
+        public MainForm()
+        {
+            InitializeComponent();
+            random = new Random();            
+            this.Text = string.Empty;
+            this.ControlBox = false;
+            this.MaximizedBounds = Screen.FromHandle(this.Handle).WorkingArea;
+        }
+
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        //Methods
+        private Color SelectThemeColor()
+        {
+            int index = random.Next(ThemeColor.ColorList.Count);
+            while (tempIndex == index)
+            {
+                index = random.Next(ThemeColor.ColorList.Count);
+            }
+            tempIndex = index;
+            string color = ThemeColor.ColorList[index];
+            return ColorTranslator.FromHtml(color);
+        }
+
+        private void ActivateButton(object btnSender)
+        {
+            if (btnSender != null)
+            {
+                if (currentButton != (Button)btnSender)
+                {
+                    DisableButton();
+                    Color color = SelectThemeColor();
+                    currentButton = (Button)btnSender;
+                    currentButton.BackColor = color;
+                    currentButton.ForeColor = Color.White;
+                    currentButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                    TitlePanel.BackColor = color;
+                    LogoPanel.BackColor = ThemeColor.ChangeColorBrightness(color, -0.3);
+                    ThemeColor.PrimaryColor = color;
+                    ThemeColor.SecondaryColor = ThemeColor.ChangeColorBrightness(color, -0.3);                    
+                }
+            }
+        }
+
+        private void DisableButton()
+        {
+            foreach (Control previousBtn in MenuPanel.Controls)
+            {
+                if (previousBtn.GetType() == typeof(Button))
+                {
+                    previousBtn.BackColor = Color.FromArgb(51, 51, 76);
+                    previousBtn.ForeColor = Color.Gainsboro;
+                    previousBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                }
+            }
+        }
+
+        private void OpenChildForm(Form childForm, object btnSender)
+        {
+            if (activeForm != null)
+                activeForm.Close();
+            ActivateButton(btnSender);
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            this.WorkspacePanel.Controls.Add(childForm);
+            this.WorkspacePanel.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+            TitlePanel.Text = childForm.Text;
+
+        }
+
+        private void AddBotButton_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void TitlePanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void MaxButton_Click(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Normal)
+            {
+                this.WindowState = FormWindowState.Maximized;
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+        }
+
+        private void MinButton_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+    }
+}
